0ef5ac1f1f4fb7ae4b5a00d07066724fc24cb3f9
Heitor Filizzola <76500311+heitorfilizzola@users.noreply.github.com>
2024-05-21 19:14:54 -0300

    v3.5

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-/*function abrirPg(url){
-    window.location.href("file:/login/index.html", '_self');
-}*/
+window.onscroll = () => {
+    const navbarEl = document.querySelector("#frame1")
+      if (window.scrollY > 0) navbarEl.style.background = "white"
+      else navbarEl.style.background = "rgba(0,0,0,0)"
+      
+      const navbarTxt = document.querySelector(".btns")
+      if (window.scrollY > 0) {
+          navbarTxt.style.color = "#3ca028" 
+          navbarTxt.style.textShadow = "none"
+      }else {
+          navbarTxt.style.color = "white"
+          navbarTxt.style.textShadow = "5px 5px 5px rgb(90, 90, 90)"
+      }
+    
+    
+    }
+    
