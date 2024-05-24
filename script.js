2575088350c58e2e73dac1adfe98f0886c1abfb4
Heitor Filizzola <76500311+heitorfilizzola@users.noreply.github.com>
2024-05-23 23:02:30 -0300

    v3.7

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 window.onscroll = () => {
     const navbarEl = document.querySelector("#frame1")
-      if (window.scrollY > 0) navbarEl.style.background = "white"
-      else navbarEl.style.background = "rgba(0,0,0,0)"
-      
-      const navbarTxt = document.querySelector(".btns")
       if (window.scrollY > 0) {
-          navbarTxt.style.color = "#3ca028" 
-          navbarTxt.style.textShadow = "none"
-      }else {
-          navbarTxt.style.color = "white"
-          navbarTxt.style.textShadow = "5px 5px 5px rgb(90, 90, 90)"
+        navbarEl.style.background = "white"
+      }else{
+        navbarEl.style.background = "rgba(0,0,0,0)"
       }
+
+      const navbarTxts = document.querySelectorAll(".btns");
+      navbarTxts.forEach(btn => {
+          if (window.scrollY > 0) {
+              btn.style.color = "#3ca028";
+              btn.style.textShadow = "none";
+          } else {
+              btn.style.color = "white";
+              btn.style.textShadow = "5px 5px 5px rgb(90, 90, 90)";
+          }
+      });
     
-    
+    const headerImg = document.querySelector(".h_img")
+        if (window.scrollY > 0) {
+            headerImg.src = "imgs/logoverde.png"
+        }else {
+            headerImg.src = "imgs/logocinza.png"
+        }
+
     }
     
