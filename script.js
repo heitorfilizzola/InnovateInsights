322df00201cb57c7452291632397ff637ff76474
Heitor Filizzola <76500311+heitorfilizzola@users.noreply.github.com>
2024-05-28 11:45:53 -0300

    v3.8.1

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 window.onscroll = () => {
-    const navbarEl = document.querySelector("#frame1")
-      if (window.scrollY > 0) {
-        navbarEl.style.background = "white"
-      }else{
-        navbarEl.style.background = "rgba(0,0,0,0)"
-      }
-
-      const navbarTxts = document.querySelectorAll(".btns");
-      navbarTxts.forEach(btn => {
-          if (window.scrollY > 0) {
-              btn.style.color = "#3ca028";
-              btn.style.textShadow = "none";
-          } else {
-              btn.style.color = "white";
-              btn.style.textShadow = "5px 5px 5px rgb(90, 90, 90)";
-          }
-      });
-    
-    const headerImg = document.querySelector(".h_img")
-        if (window.scrollY > 0) {
-            headerImg.src = "imgs/logoverde.png"
-        }else {
-            headerImg.src = "imgs/logocinza.png"
-        }
-
-        const navgrad = document.querySelector(".pre_login1")
-      if (window.scrollY > 0) {
-        navgrad.style.background = "rgba(0,0,0,0)"
-      }else{
-        navgrad.style.background = "linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(164,164,164,1) 78%, rgba(164,164,164,1) 95%, rgba(164,164,164,1) 100%);"
-      }
+  const navbarEl = document.querySelector("#frame1")
+    if (window.scrollY > 0) {
+      navbarEl.style.background = "white"
+    } else{
+      navbarEl.style.background = "linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(164,164,164,1) 78%, rgba(164,164,164,1) 95%, rgba(164,164,164,1) 100%)"
     }
-    
+
+    const navbarTxts = document.querySelectorAll(".btns");
+    navbarTxts.forEach(btn => {
+        if (window.scrollY > 0) {
+            btn.style.color = "#3ca028";
+            btn.style.textShadow = "none";
+        } else {
+            btn.style.color = "white";
+            btn.style.textShadow = "5px 5px 5px rgb(90, 90, 90)";
+        }
+    });
+  
+  const headerImg = document.querySelector(".h_img")
+      if (window.scrollY > 0) {
+          headerImg.src = "imgs/logoverde.png"
+      }else {
+          headerImg.src = "imgs/logocinza.png"
+      }
+
+  }
+  
